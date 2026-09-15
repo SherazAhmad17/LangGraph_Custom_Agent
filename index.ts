34b72a3d213140883fe85087ffeefbae2dba7266
Sheraz Ahmad <sherazahmad7434@gmail.com>
2026-09-15 23:18:34 +0500

    toolCalling WebSearch is implemented

--- index.ts
+++ index.ts
@@ -1,15 +1,14 @@
+import { AIMessage } from "@langchain/core/messages";
 import { ChatGroq } from "@langchain/groq";
 import { MessagesAnnotation, StateGraph } from "@langchain/langgraph";
 import { ToolNode } from "@langchain/langgraph/prebuilt";
 import { TavilySearch } from "@langchain/tavily";
 import readline from "node:readline/promises";
 
-
-const searchTool = new TavilySearch({ maxResults: 3, topic: "general", });
-
+const searchTool = new TavilySearch({ maxResults: 3, topic: "general" });
 
 const tools = [searchTool];
-const toolNode = new ToolNode(tools)
+const toolNode = new ToolNode(tools);
 
 const llm = new ChatGroq({
   model: "openai/gpt-oss-120b",
@@ -22,31 +21,36 @@ const llm = new ChatGroq({
   apiKey: process.env.GROQ_API_KEY,
 }).bindTools(tools);
 
-
-
 // HERE ARE THE STEPS HAVE TO FOLLOW
 //1. Defining a node
 
-async function callModel(state : typeof MessagesAnnotation.State) {
+async function callModel(state: typeof MessagesAnnotation.State) {
   console.log("calling llm");
-  const response = await llm.invoke(state.messages)
-  return {messages: [...state.messages, response]};
+  const response = await llm.invoke(state.messages);
+  return { messages: [...state.messages, response] };
 }
 
-function shouldContinue(state : typeof MessagesAnnotation.State){
-  console.log('state', state);
-  
-  return '__end__';
+function shouldContinue(state: typeof MessagesAnnotation.State) {
+  const finalMessage = state.messages[state.messages.length - 1];
+
+  if (
+    AIMessage.isInstance(finalMessage) &&
+    finalMessage.tool_calls &&
+    finalMessage.tool_calls.length > 0
+  ) {
+    return "tools";
+  }
+
+  return "__end__";
 }
 
 //Build a graph
 const workflow = new StateGraph(MessagesAnnotation)
   .addNode("agent", callModel)
-  .addNode('tools', toolNode)
+  .addNode("tools", toolNode)
   .addEdge("__start__", "agent")
-  .addEdge("agent", "__end__")
-  .addConditionalEdges('agent', shouldContinue)
-  
+  .addEdge("tools", "agent")
+  .addConditionalEdges("agent", shouldContinue);
 
 //compile the graph
 const app = workflow.compile();
@@ -68,9 +72,8 @@ async function main(): Promise<void> {
       const finalMessage = finalState.messages[finalState.messages.length - 1];
 
       console.log(finalMessage?.content);
-      
 
-      console.log("finalState", finalState);
+      // console.log("finalState", finalState);
 
       // console.log("you ask this: ", userInput);
     }
